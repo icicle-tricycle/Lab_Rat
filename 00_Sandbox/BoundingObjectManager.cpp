4db07f05de4d58c48f92f374a3e7c22b0f1c7523
Caitlin Rips (RIT Student) <crr6381@ad.rit.edu>
2016-04-20 17:04:35 -0400

    Pseudocode for collision detection and resolution

--- 00_Sandbox/BoundingObjectManager.cpp
+++ 00_Sandbox/BoundingObjectManager.cpp
@@ -61,8 +61,24 @@ void BoundingObjectManager::renderBO(int index)
 
 void BoundingObjectManager::checkCollisions()
 {
+	//run through array of objects and see what's colliding
+
+	//if 2 objects are colliding...
+
+	//make sure the indexes aren't identical (object colliding with itself) and do nothing if they are
+
+	//call resolveCollision on the indexes of the colliding objects
 }
 
 void BoundingObjectManager::resolveCollision(int index1, int index2)
 {
+	//for each object...
+	
+	//calculate vector from center of one object to the other
+
+	//multiply by -1 so vector points away from colliding object
+
+	//normalize
+
+	//add vector to object's velocity so it moves away from the other object and "bounces" off
 }
